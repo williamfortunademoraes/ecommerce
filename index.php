6b9857137dc7b52696de1ac7df4d004672b6f85b
William Fortuna <william.fortuna@yahoo.com.br>
2019-08-05 02:55:01 -0300

    PROJETO COM ERRO

--- index.php
+++ index.php
@@ -2,16 +2,21 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Hcode\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
-	echo "OK";
+	
+	$page = new Page();
 
+	$page->setTpl("index");
+	
 });
 
 $app->run();
 
- ?>
+?>
